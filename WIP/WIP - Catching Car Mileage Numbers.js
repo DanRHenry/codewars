8f5f61d27a54dc9e2a00fd1c435e71c7cf1deda2
DanRHenry <114960108+DanRHenry@users.noreply.github.com>
2024-05-05 10:41:05 -0400

    Passing basic tests, failing advanced listed in code

--- WIP/WIP - Catching Car Mileage Numbers.js
+++ WIP/WIP - Catching Car Mileage Numbers.js
@@ -53,48 +53,44 @@ Interesting numbers are 3-or-more digit numbers that meet one or more of the fol
 ‡ For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
 
 */ let numString = number.toString();
-let flag = {};
+  let flag = {};
   if (numString.length >= 3 || numString === "99" || numString === "98") {
-    // function testForSequentialOrPalindrome() {
-
-
     // Test for Any digit followed by all zeros: 100, 90000
     function testForTrailingZeroes() {
       let previous;
       for (let i = 2; i < numString.length; i++) {
         let comparativeValue = Number(numString[1]);
         if (comparativeValue === 0 || comparativeValue === 9) {
-        let currentValue = Number(numString[i]);
-        if (i != numString.length - 1) {
-          // if (currentValue === ) {
-          //   previous = 0;
-          // }
-          if (currentValue === comparativeValue) {
-            previous = comparativeValue;
-          } else {
-            console.log("here")
-            break;
-          }
-          // if (currentValue === comparativeValue && previous === comparativeValue) {
-          //   previous = comparativeValue;
-          // }
-          // else if (currentValue === 9) {
-          //   flag.testForAllFollowingZeroes = false;
+          let currentValue = Number(numString[i]);
+          if (i != numString.length - 1) {
+            // if (currentValue === ) {
+            //   previous = 0;
+            // }
+            if (currentValue === comparativeValue) {
+              previous = comparativeValue;
+            } else {
+              // console.log("here");
+              break;
+            }
+            // if (currentValue === comparativeValue && previous === comparativeValue) {
+            //   previous = comparativeValue;
+            // }
+            // else if (currentValue === 9) {
+            //   flag.testForAllFollowingZeroes = false;
 
-          //   break;
-          // }
-        } else if (currentValue + 2 === 10 || currentValue + 1 === 10) {
-          flag.upcomingTrailingZeroes = true;
-        } else if (currentValue === 0 && previous === 0) {
-          flag.trailingZeroes = true;
+            //   break;
+            // }
+          } else if (currentValue + 2 === 10 || currentValue + 1 === 10) {
+            flag.upcomingTrailingZeroes = 1;
+          } else if (currentValue === 0 && previous === 0) {
+            flag.trailingZeroes = 2;
+          }
         }
       }
-
-    }
-      if (flag.trailingZeroes === true) {
+      if (flag.trailingZeroes === 2) {
         console.log("all trailing zeroes true");
       }
-      if (flag.upcomingTrailingZeroes === true) {
+      if (flag.upcomingTrailingZeroes === 1) {
         console.log("upcoming all trailing zeroes true");
       }
     }
@@ -107,20 +103,20 @@ let flag = {};
         let nextValue = Number(numString[i + 1]);
         if (i < numString.length - 1) {
           // check that all previous values are incrementing
-          if (currentValue === nextValue - 1 && i < numString.length -2) {
+          if (currentValue === nextValue - 1 && i < numString.length - 2) {
             previous = currentValue;
           } else if (i >= numString.length - 2) {
             let penultimateNumber = Number(numString[numString.length - 2]);
             let ultimateNumber = Number(numString[numString.length - 1]);
             // look for upcoming ascending number
             if (penultimateNumber === ultimateNumber - 1) {
-              flag.ascending = true;
+              flag.ascending = 2;
               console.log("flag.ascending:", flag.ascending);
             } else if (
               penultimateNumber === ultimateNumber ||
               penultimateNumber === ultimateNumber + 1
             ) {
-              flag.upcomingAscending = true;
+              flag.upcomingAscending = 1;
               console.log("flag.upcomingAscending:", flag.upcomingAscending);
             }
           } else {
@@ -130,60 +126,86 @@ let flag = {};
       }
     }
 
+    // function testForDecrementing() {
+    //   let previous;
+    //   for (let i = 0; i < numString.length - 1; i++) {
+    //     //Test for the digits are sequential, decrementing‡: 4321
+    //     let currentValue = Number(numString[i]);
+    //     let nextValue = Number(numString[i + 1]);
+
+    //     if (i < numString.length - 1) {
+    //       if (currentValue === nextValue + 1 && i < numString.length - 2) {
+    //         previous = currentValue;
+
+    //       } else if (i >= numString.length - 2) {
+    //         previous = numString[numString.length - 3];
+    //         let idealEnding = Number(
+    //           (previous - 1).toString() + (previous - 2).toString()
+    //         );
+    //         let penultimateNumber = Number(numString[numString.length - 2]);
+    //         let ultimateNumber = Number(numString[numString.length - 1]);
+    //         let finalTwo = Number(
+    //           numString[numString.length - 2] + numString[numString.length - 1]
+    //         );
+
+    //         let finalNumPosOne = Number(finalTwo.toString()[0]);
+    //         let finalNumPosTwo = Number(finalTwo.toString()[1]);
+
+    //         if (previous - 1 === finalNumPosOne) {
+    //           previous = finalNumPosOne;
+    //         }
+    //         if (previous - 1 === finalNumPosTwo) {
+    //           console.log("descending sequence");
+    //         }
+    //         if (penultimateNumber === ultimateNumber + 1) {
+    //           flag.descending = true;
+    //           console.log("descending is true");
+    //         } else if (
+    //           // true === true
+    //           idealEnding - 1 === finalTwo ||
+    //           idealEnding - 2 === finalTwo
+    //         ) {
+    //           flag.upcomingDescending = true;
+    //           console.log("flag.upcomingDescending:", flag.upcomingDescending);
+    //         } else {
+    //           break;
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
+
     function testForDecrementing() {
-      let previous;
       for (let i = 0; i < numString.length - 1; i++) {
-        //Test for the digits are sequential, decrementing‡: 4321
+        let check;
         let currentValue = Number(numString[i]);
         let nextValue = Number(numString[i + 1]);
-
-        console.log("current:", currentValue, "next", nextValue);
-        if (i < numString.length - 1) {
-          if (currentValue === nextValue + 1 && i < numString.length -2) {
-            previous = currentValue;
-            // } else if (i >= numString.length - 2 && previous === currentValue) {
-          } else if (i >= numString.length - 2) {
-            console.log("previous:",previous)
-            previous = numString[numString.length -3];
-            let idealEnding = Number((previous -1).toString() + (previous -2).toString());
-            let penultimateNumber = Number(numString[numString.length - 2]);
-            let ultimateNumber = Number(numString[numString.length - 1]);
-            let finalTwo = Number(
-              numString[numString.length - 2] + numString[numString.length - 1]
-            );
-            console.log("finalTwo:",finalTwo);
-
-            let finalNumPosOne = Number((finalTwo).toString()[0]);
-            let finalNumPosTwo = Number((finalTwo).toString()[1]);
-            console.log("finalNumPOne:",finalNumPosOne);
-            // console.log("finalNumPTwo:", finalNumPosTwo);
-
-            if (previous - 1 === finalNumPosOne) {
-              previous = finalNumPosOne
-            }
-            if (previous -1 === finalNumPosTwo) {
-              console.log("descending sequence")
-            }
-            if (penultimateNumber === ultimateNumber + 1) {
-              flag.descending = true;
-              console.log("descending is true");
-            } else if (
-              // true === true
-              idealEnding - 1 === finalTwo || idealEnding - 2 === finalTwo
-            ) {
-              // console.log(idealEnding, finalTwo)
-              // console.log("here")
-              // console.log(Number(numString[numString.length - 3])+1)
-              // console.log(previous, finalNumPosOne, finalNumPosTwo)
-            // } else if (
-            //   penultimateNumber === finalNumPosOne ||
-            //   penultimateNumber === finalNumPosTwo
-            // ) {
-              flag.upcomingDescending = true;
-              console.log("flag.upcomingDescending:", flag.upcomingDescending);
-            } else {
-              break;
-            }
+        if (i < numString.length - 2) {
+          if (currentValue === nextValue + 1) {
+            check = true;
+          } else if (currentValue !== nextValue + 1) {
+            check = false;
+          } else {
+            break;
+          }
+        }
+        if (i >= numString.length - 2) {
+          let finalCheck = Number(
+            (Number(numString[0]) - i).toString() +
+              (Number(numString[1]) - i).toString()
+          );
+          let finalNumber = Number(
+            numString[numString.length - 2] + numString[numString.length - 1]
+          );
+          if (finalCheck === finalNumber) {
+            flag.descending = 2;
+            console.log("flag.descending:", flag.descending);
+          } else if (
+            finalCheck === finalNumber + 1 ||
+            finalCheck === finalNumber + 2
+          ) {
+            flag.upcomingDescending = 1;
+            console.log("flag.upcomingDescending:", flag.upcomingDescending);
           }
         }
       }
@@ -202,7 +224,7 @@ let flag = {};
         backward += numString[j - 1];
       }
       if (forward === backward) {
-        flag.palindrome = true;
+        flag.palindrome = 2;
         console.log("Palindrome Found");
       }
 
@@ -219,114 +241,131 @@ let flag = {};
         Number(firstTwoReversed) === lastTwo + 1 ||
         Number(firstTwoReversed) === lastTwo + 2
       ) {
-        flag.upcomingPalindrome = true;
+        flag.upcomingPalindrome = 1;
         console.log("Upcoming Palindrome Found");
       }
     }
-
-    // }
 
     // Test for the digits match one of the values in the awesomePhrases array
     function checkAwesomePhrases() {
       for (let i = 0; i < awesomePhrases?.length; i++) {
         if (awesomePhrases[i].toString() === numString) {
           console.log("matched", awesomePhrases[i], "&", numString);
-          return 2;
+          flag.awesomePhrases = 2;
         } else if (
           awesomePhrases[i] === number + 2 ||
           awesomePhrases[i] === number + 1
         ) {
           console.log("2 mile warning", awesomePhrases[i], "&", numString);
-          return 1;
+          flag.upcomingAwesomePhrases = 1;
         }
       }
     }
-
-    // }
 
     //Test for Every digit is the same number: 1111
-    function testForAllSame() {
-      let same;
-      let comparativeValue = Number(numString[0]);
-      for (let i = 1; i < numString.length - 1; i++) {
-        if (Number(numString[i]) === comparativeValue && i < numString.length - 2) {
-        // if (numString[i] === numString[i + 1] && i != numString.length - 1) {
-          // same = true;
-          // console.log(same)
-        // } else if (same === true) {
-          if (i === numString.length - 2) {
-            // console.log("here")
+    //   function testForAllSame() {
+    //     let comparativeValue = Number(numString[0]);
+    //     let check;
+    //     for (let i = 1; i < numString.length - 1; i++) {
+    //       if (Number(numString[i]) === comparativeValue && (i != numString.length-1 || i != numString.length-2)) {
+    //       if (Number(numString[i]) === comparativeValue && i < numString.length - 2) {
+    //         // check = Number(numString[i]);
+    //         if (i === numString.length - 2) {
+    //           let penultimateNumber = numString[numString.length - 2];
+    //           let ultimateNumber = numString[numString.length - 1];
+    //           let finalTwo = +penultimateNumber * 10 + +ultimateNumber;
+    //           let upcomingOne = (finalTwo + 1).toString();
+    //           let upcomingTwo = (finalTwo + 2).toString();
+    //           if (
+    //             upcomingOne[0] === upcomingOne[1] ||
+    //             upcomingTwo[0] === upcomingTwo[1]
+    //           ) {
+    //             console.log("two mile warning same");
+    //             flag.upcomingAllTheSame = true;
+    //           }
+    //         }
+    //       }
 
-            let penultimateNumber = numString[numString.length - 2];
-            let ultimateNumber = numString[numString.length - 1];
-            let finalTwo = +penultimateNumber * 10 + +ultimateNumber;
-            let upcomingOne = (finalTwo + 1).toString();
-            let upcomingTwo = (finalTwo + 2).toString();
-            if (
-              upcomingOne[0] === upcomingOne[1] ||
-              upcomingTwo[0] === upcomingTwo[1]
-            ) {
-              console.log("two mile warning same");
-              flag.upcomingAllTheSame = true;
-            }
+    //     let ultimateNumber = Number(numString[numString.length - 1])
+    //     let penultimateNumber = Number(numString[numString.length - 2])
+    //     let finalTwo = penultimateNumber.toString() + ultimateNumber.toString()
+    //     if (check != false && ultimateNumber === penultimateNumber && penultimateNumber === comparativeValue) {
+    //       console.log("all the same number");
+    //       flag.allTheSame = true;
+    //     }
+    //     if (check != false &&
+    //       ((Number(finalTwo) + 1 === (Number(comparativeValue.toString()+comparativeValue.toString())) || Number(finalTwo) + 2 === (Number(comparativeValue.toString()+comparativeValue.toString()))))) {
+    //       console.log('upcomingTrue')
+    //       flag.upcomingAllTheSame = true
+    //     }
+    //   }
+    //   else {
+    //     check === false;
+    //   }
+    //   }
+    // }
+
+    function testForAllSame() {
+      let firstNumber = numString[0];
+      let lastTwo =
+        numString[numString.length - 2] + numString[numString.length - 1];
+      let finalComparison = numString[0] + numString[0];
+      let check;
+      for (let i = 1; i < numString.length - 1; i++) {
+        if (numString[i] === firstNumber) {
+          check = true;
+        }
+        if (i < numString.length - 3 && numString[i + 1] != firstNumber) {
+          check = false;
+          break;
+        }
+        if (i >= numString.length - 2 && check === true) {
+          if (
+            check === true &&
+            (+lastTwo + 1 === +finalComparison ||
+              +lastTwo + 2 === +finalComparison)
+          ) {
+            console.log("upcomingAllSame");
+            flag.upcomingAllTheSame = 1;
+          } else if (check === true && +lastTwo[0] === +lastTwo[1]) {
+            console.log("all the same");
+            flag.AllTheSame = 2;
           }
         }
-        // else {
-        //   console.log("breaking")
-        //   break;
-        // }
-      }
-      // console.log(typeof numString[numString.length - 1], typeof numString[numString.length - 2], typeof comparativeValue)
-      let ultimateNumber = Number(numString[numString.length - 1])
-      let penultimateNumber = Number(numString[numString.length - 2])
-      // console.log((ultimateNumber +1 === comparativeValue && penultimateNumber +1 === comparativeValue) || (penultimateNumber +1 === comparativeValue && ultimateNumber + 2 === comparativeValue))
-      // console.log((comparativeValue + comparativeValue))
-      let finalTwo = penultimateNumber.toString() + ultimateNumber.toString()
-      // console.log("finalTwo",finalTwo)
-
-      if ((ultimateNumber === penultimateNumber) && (penultimateNumber === comparativeValue)) {
-        console.log("all the same number");
-        flag.allTheSame = true;
-      }
-      else if (Number(finalTwo) + 1 === (Number(comparativeValue.toString()+comparativeValue.toString())) || Number(finalTwo) + 2 === (Number(comparativeValue.toString()+comparativeValue.toString()))) {
-        // console.log(Number((finalTwo[1] +1).toString()[0]))
-      // (Number(finalTwo[1])+ 1 === comparativeValue || Number(finalTwo[1])+ 2 === comparativeValue) {
-        console.log('upcomingTrue')
-        flag.upcomingAllTheSame === true
       }
     }
 
+    testForTrailingZeroes(); // Seems to be fully working
 
-    // testForTrailingZeroes(); // Seems to be fully working
+    testForIncrementing(); //  Seems to be fully working
 
-    // testForIncrementing(); //  Seems to be fully working
+    testForDecrementing();
 
-    // testForDecrementing();
+    searchForPalindromes(); // Seems to be fully working
 
-    // searchForPalindromes(); // Seems to be fully working
-
-    // checkAwesomePhrases(); // Seems to be fully working
+    checkAwesomePhrases(); // Seems to be fully working
 
     testForAllSame(); // Seems to be fully working
-
   }
-  console.log(flag)
-
+  console.log(flag);
+  let flagArray = Object.values(flag);
+  console.log("flagArray: ", flagArray);
+  if (flagArray.includes(2)) {
+    console.log("2");
+    return 2;
+  } else if (flagArray.includes(1)) {
+    console.log("1");
+    return 1;
+  } else {
+    console.log(0);
+    return 0;
+  }
+  // for ()
 }
 
-// isInteresting(30000099, [1337, 256], 2);
-// isInteresting(2232);
-// isInteresting(21509, [1337, 256], 1);
-// isInteresting(11211, [1337, 256], 2);
-// isInteresting(1000000000000);
-isInteresting(22222219);
+isInteresting(100, [1337, 256]), 2; //should handle big numbers
+// isInteresting(67890, [1337, 256]), 2; //should handle incrementing sequences
+// isInteresting(119, [1337, 256]), 1; //should handle upcoming palindromic numbers
+// isInteresting(67888, [1337, 256]), 1; //should handle upcoming incrementing sequences
+// isInteresting(98, [1337, 256]), 1; //should handle upcoming big numbers
 
-// isInteresting(11111111, [1337, 256]); 
-// isInteresting(4321);
-// isInteresting(10000000);
-// isInteresting(4324);
-// isInteresting(4319);
-// isInteresting(3, [1337, 256], 0);
-// isInteresting(1337, [1337, 256], 1);
-// isInteresting(256, [1337, 256], 2);
-// isInteresting(11208, [1337, 256], 0);
